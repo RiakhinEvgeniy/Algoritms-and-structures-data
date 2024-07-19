ed57e64c05e72b63b16abcd623f3dfd2ae0c4145
Riakhin Evgeniy <apelles.odesskiy@gmail.com>
2024-07-19 14:31:42 +0200

    Added counter character in string

--- src/different_algorithms/CountRepeatCharacter.java
+++ src/different_algorithms/CountRepeatCharacter.java
@@ -1,11 +1,22 @@
 package different_algorithms;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CountRepeatCharacter {
     public static void main(String[] args) {
-
+        String str = "Map<Character, Integer> map = countRepeatCharacters(str);";
+        Map<Character, Integer> map = countRepeatCharacters(str);
+        System.out.println(map);
     }
 
-    protected static void countRepeatCharacters(String str) {
+    protected static Map<Character, Integer> countRepeatCharacters(String str) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            map.compute(ch, (k, v) -> v == null ? 1 : v + 1);
+        }
 
+        return map;
     }
 }
