af1f6b54e078b31cd2433145136d5c6bcaaac3b5
Riakhin Evgeniy <apelles.odesskiy@gmail.com>
2024-07-19 12:56:35 +0200

    Fix warning.

--- src/different_algorithms/DifferentAlgorithmsApp.java
+++ src/different_algorithms/DifferentAlgorithmsApp.java
@@ -39,9 +39,9 @@ public class DifferentAlgorithmsApp {
     }
 
     static void printValuesArray(int[][] array) {
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                System.out.print(array[i][j] + " ");
+        for (int[] ints : array) {
+            for (int anInt : ints) {
+                System.out.print(anInt + " ");
             }
             System.out.println();
         }
