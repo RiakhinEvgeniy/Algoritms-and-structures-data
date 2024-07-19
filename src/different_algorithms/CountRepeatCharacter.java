d56f22986421fdc786cdf91d43cbaa40e1478953
Riakhin Evgeniy <apelles.odesskiy@gmail.com>
2024-07-19 14:21:50 +0200

    Added printing by order running

---
+++ src/different_algorithms/CountRepeatCharacter.java
@@ -0,0 +1,11 @@
+package different_algorithms;
+
+public class CountRepeatCharacter {
+    public static void main(String[] args) {
+
+    }
+
+    protected static void countRepeatCharacters(String str) {
+
+    }
+}
--- src/different_algorithms/DifferentAlgorithmsApp.java
+++ src/different_algorithms/DifferentAlgorithmsApp.java
@@ -2,30 +2,33 @@ package different_algorithms;
 
 public class DifferentAlgorithmsApp {
 
-    private final static int KEY = 400;
+    private final static int KEY_FOR_FIND_VALUE = 41;
 
     public static void main(String[] args) {
         int[][] array = fillArray();
 
         printArrayWithBrackets(array);
-        System.out.println("=====================================");
+        System.out.println("=====================================\n");
+
+        printOrderRunning(array);
+        System.out.println("=====================================\n");
 
         printValuesArray(array);
-        System.out.println("=====================================");
+        System.out.println("=====================================\n");
 
         printCountRowsAndColumns(array);
-        System.out.println("=====================================");
+        System.out.println("=====================================\n");
 
         System.out.println(findByKey(array));
     }
 
     static int[][] fillArray() {
         return new int[][]{
-                {1, 3, 5, 7},
-                {8, 11, 16, 17},
-                {22, 28, 29, 35},
-                {41, 75, 89, 102},
-                {123, 124, 189, 400},
+                {1, 3, 5, 7, 8, 11, 13, 16, 20},
+                {2, 5, 6, 8, 9, 12, 14, 17, 22},
+                {3, 6, 7, 9, 10, 13, 17, 19, 24},
+                {27, 75, 89, 102, 105, 123, 127, 135, 140},
+                {41, 200, 205, 207, 230, 235, 241, 242, 253},
         };
     }
 
@@ -57,11 +60,11 @@ public class DifferentAlgorithmsApp {
         int jCol = col - 1;
         System.out.printf("row: %d, col: %d\n", row, col);
         while (iRow < row && jCol >= 0) {
-            if (array[iRow][jCol] == KEY) {
+            if (array[iRow][jCol] == KEY_FOR_FIND_VALUE) {
                 return true;
             }
 
-            if (array[iRow][jCol] > KEY) {
+            if (array[iRow][jCol] > KEY_FOR_FIND_VALUE) {
                 jCol--;
             } else {
                 iRow++;
@@ -73,5 +76,30 @@ public class DifferentAlgorithmsApp {
     static void printCountRowsAndColumns(int[][] array) {
         System.out.println("Rows = " + array.length);
         System.out.println("Columns = " + array[0].length);
+    }
+
+    static void printOrderRunning(int[][] array) {
+        if (array == null || array.length == 0 || array[0].length == 0) {
+            return;
+        }
+        int row = array.length;
+        int col = array[0].length;
+        int iRow = 0;
+        int jCol = col - 1;
+        System.out.printf("row: %d, col: %d\n", row, col);
+        while (iRow < row && jCol >= 0) {
+            if (array[iRow][jCol] == KEY_FOR_FIND_VALUE) {
+                System.out.printf("array[iRow][jCol] == KEY -> array[%d][%d] = %d = %d; \n", iRow, jCol, array[iRow][jCol], KEY_FOR_FIND_VALUE);
+                return;
+            }
+
+            if (array[iRow][jCol] > KEY_FOR_FIND_VALUE) {
+                System.out.printf("array[iRow][jCol] > KEY -> array[%d][%d] = %d > %d; \n", iRow, jCol, array[iRow][jCol], KEY_FOR_FIND_VALUE);
+                jCol--;
+            } else {
+                System.out.printf("array[iRow][jCol] < KEY -> array[%d][%d] = %d < %d; \n", iRow, jCol, array[iRow][jCol], KEY_FOR_FIND_VALUE);
+                iRow++;
+            }
+        }
     }
 }
